9a64cb9eb571faf8484836456c21b3be4c53be2e
Brandon <brandonpretelt93@gmail.com>
2022-01-17 21:52:05 -0700

    :sparkles: nav work

--- unit01/js/script.js
+++ unit01/js/script.js
@@ -1,3 +1,26 @@
 // TODO:  replace with server php code in the future
 const dateSpan = document.querySelector('.date');
 dateSpan.textContent = new Date().getFullYear();
+
+const closeBtn = document.querySelector('.close-menu');
+const openBtn = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.mobile-menu');
+const dropdown = document.querySelector('.open-dropdown');
+
+openBtn.addEventListener('click', () => {
+    menu.classList.toggle('open-menu');
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.toggle('open-menu');
+    if (dropdown.classList.contains('rotate')) {
+        dropdown.classList.remove('rotate');
+    }
+});
+
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('rotate');
+    document
+        .querySelector('.drop-down')
+        .classList.toggle('open-dropdown-links');
+});
