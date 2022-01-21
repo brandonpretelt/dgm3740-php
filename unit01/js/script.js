71711dcadde08ad25989791405ba1eb2f9f9cf7f
Brandon <brandonpretelt93@gmail.com>
2022-01-21 01:43:04 -0700

    :hammer: fixed

--- unit01/js/script.js
+++ unit01/js/script.js
@@ -6,6 +6,7 @@ const closeBtn = document.querySelector('.close-menu');
 const openBtn = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.mobile-menu');
 const dropdown = document.querySelector('.open-dropdown');
+const dropdownMenu = document.querySelector('.drop-down');
 
 openBtn.addEventListener('click', () => {
     menu.classList.toggle('open-menu');
@@ -15,12 +16,20 @@ closeBtn.addEventListener('click', () => {
     menu.classList.toggle('open-menu');
     if (dropdown.classList.contains('rotate')) {
         dropdown.classList.remove('rotate');
+        document
+            .querySelector('.drop-down')
+            .classList.remove('open-dropdown-links');
     }
 });
 
 dropdown.addEventListener('click', () => {
     dropdown.classList.toggle('rotate');
-    document
-        .querySelector('.drop-down')
-        .classList.toggle('open-dropdown-links');
+    dropdownMenu.classList.toggle('open-dropdown-links');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target !== dropdown) {
+        dropdown.classList.remove('rotate');
+        dropdownMenu.classList.remove('open-dropdown-links');
+    }
 });
