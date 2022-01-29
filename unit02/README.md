3efa95de65d5a9d1838d6c627dc1f602be5e42ea
Brandon <brandonpretelt93@gmail.com>
2022-01-28 22:46:46 -0700

    :tada: unit02 commit

---
+++ unit02/README.md
@@ -0,0 +1,17 @@
+# Assignment Requirements:
+
+## Site Header
+
+-   [x] Build custom logo
+
+    -   [x] 1x with label
+    -   [x] 2x with label
+    -   [x] 3x with label
+
+-   [x] Phone number in header using tel:
+-   [ ] background color of header should be full page width
+-   [ ] header should be a maximum of 1200px
+
+## Nav & Search
+
+-   [ ]
---
+++ unit02/js/script.js
@@ -0,0 +1,41 @@
+// TODO:  replace with server php code in the future
+const dateSpan = document.querySelector('.date');
+dateSpan.textContent = new Date().getFullYear();
+
+const closeBtn = document.querySelector('.close-menu');
+const openBtn = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.mobile-menu');
+const dropdown = document.querySelector('.open-dropdown');
+const dropdownMenu = document.querySelector('.drop-down');
+const liToggle = document.querySelector('.li-heading');
+
+openBtn.addEventListener('click', () => {
+    menu.classList.toggle('open-menu');
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.toggle('open-menu');
+    if (dropdown.classList.contains('rotate')) {
+        dropdown.classList.remove('rotate');
+        document
+            .querySelector('.drop-down')
+            .classList.remove('open-dropdown-links');
+    }
+});
+
+liToggle.addEventListener('click', () => {
+    dropdown.classList.toggle('rotate');
+    dropdownMenu.classList.toggle('open-dropdown-links');
+});
+
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('rotate');
+    dropdownMenu.classList.toggle('open-dropdown-links');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target !== dropdown && e.target !== liToggle) {
+        dropdown.classList.remove('rotate');
+        dropdownMenu.classList.remove('open-dropdown-links');
+    }
+});
