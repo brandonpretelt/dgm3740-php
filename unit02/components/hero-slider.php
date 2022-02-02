bbd65d6dd5bbeeb8c8792e7f1ce6d113fa80a986
brandon <brandonpretelt93@gmail.com>
2022-02-01 22:48:28 -0700

    :sparkles: modularized slider

---
+++ unit02/components/hero-slider.php
@@ -0,0 +1,33 @@
+<div class="slider-container">
+                    <div class="image-slider">
+                        <picture>
+                            <source srcset="images/slider-images/slide_1h.jpg" media="(min-width: 700px)"
+                                height="350px">
+                            <img srcset="images/slider-images/slide_1s.jpg" alt="cruise 1"
+                                src="images/slider-images/slide_1s.jpg" loading="lazy">
+                        </picture>
+
+                        <picture>
+                            <source srcset="images/slider-images/slide_2h.jpg" media="(min-width: 700px)"
+                                height="350px">
+                            <img srcset="images/slider-images/slide_2s.jpg" alt="cruise 2"
+                                src="images/slider-images/slide_2s.jpg" loading="lazy">
+                        </picture>
+
+                        <picture>
+                            <source srcset="images/slider-images/slide_3h.jpg" media="(min-width: 700px)"
+                                height="350px">
+                            <img srcset="images/slider-images/slide_3s.jpg" alt="cruise 3"
+                                src="images/slider-images/slide_3s.jpg" loading="lazy">
+                        </picture>
+
+                        <picture>
+                            <source srcset="images/slider-images/slide_4h.jpg" media="(min-width: 700px)"
+                                height="350px">
+                            <img srcset="images/slider-images/slide_4s.jpg" alt="cruise 4"
+                                src="images/slider-images/slide_4s.jpg" loading="lazy">
+                        </picture>
+
+                    </div>
+                    <div class="image-slider-dots-container" aria-label="image slider nav"></div>
+                </div>
