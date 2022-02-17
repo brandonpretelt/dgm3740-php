723269491b9ef68a68dbbf4dad02895676dd1d8e
brandon <brandonpretelt93@gmail.com>
2022-02-17 11:10:58 -0700

    :construction:

--- unit02/agent-2.php
+++ unit02/agent-2.php
@@ -1,3 +1,38 @@
 <?php $title='Agent 2'; include('includes/_top.php') ?>
-<h1> Agent 2 </h1>
+<section id="agent">
+    <h1>Wendy Wadsworth 2</h1>
+    <figure>
+        <img src="#" alt="picture of the agent">
+    </figure>
+    <article>
+    <div class="contact-info">
+        <h3>Phone:</h3>
+    <span class="phone-number">
+            <a href="tel:800-555-1234">
+            1-800-555-1234 ext. 12
+            </a>
+        </span>
+        <h3>Working</h3>
+    <span class="days-working">
+        Monday, Wednesday, Friday
+    </span>
+    <h3>Destination Specialization:</h3>
+    <span class="destinations">
+        Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas, Caribbean Cruises. 
+    </span>
+    <h3>Office Hours:</h3>
+    <span class="hours">
+        8:00am - 6:00pm
+    </span>
+    </div>
+    <div class="agent-bio">
+        <h3>Biography</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sint a sequi veniam, error quidem earum officia quasi quos unde.</p>
+    </div>
+    <div class="customer-comment">
+        Customer Comment
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate minima ab. Enim possimus expedita optio sequi labore. Perspiciatis, sint!</p>
+    </div>
+    </article>
+</section>
 <?php include('includes/_bottom.php') ?>
