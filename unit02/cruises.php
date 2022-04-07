daa9bc39ccfbf1e98c941aa733a988bc3b23bcb1
Brandon <brandonpretelt93@gmail.com>
2022-04-07 15:55:49 -0600

    :hammer: :bento:

--- unit02/cruises.php
+++ unit02/cruises.php
@@ -4,21 +4,22 @@
     <div class="cruise-container">
         <article class="article-card__cruises">
             <figure class="card-content__cruises">
-                    <img src="images/blog-images/family.jpg" alt="family image" loading="lazy">
+                    <img src="images/blog-images/cruise.jpg" alt="family image" loading="lazy">
             </figure>
             <div>
-                <h2> Australia & New Zealand Cruises from $999! </h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis ullam consequuntur nesciunt fugiat magni explicabo aut similique dicta rerum? Sequi aperiam atque sit odit earum! Facilis perferendis adipisci incidunt? </p>
+                <h2> 2024 Around the World Cruise </h2>
+                <p> On this 180-day global odyssey departing Los Angeles on January 14, 2024, you’re invited to join us aboard small and luxurious Insignia. 
+ </p>
                 <a href="#" class="btn large-btn">Read More</a>
             </div>
         </article>
         <article class="article-card__cruises">
             <figure class="card-content__cruises">
-                    <img src="images/blog-images/family.jpg" alt="family image" loading="lazy">
+                    <img src="images/blog-images/cruise-2.jpg" alt="family image" loading="lazy">
             </figure>
             <div>
-                <h2> Australia & New Zealand Cruises from $999! </h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis ullam consequuntur nesciunt fugiat magni explicabo aut similique dicta rerum? Sequi aperiam atque sit odit earum! Facilis perferendis adipisci incidunt? </p>
+                <h2>Small, Luxurious Ships </h2>
+                <p> Our personalized service means our staff and crew attend to your every request with a smile. Welcome to the luxurious Insignia. </p>
                             <a href="#" class="btn large-btn">Read More</a>
             </div>
 
@@ -26,11 +27,11 @@
         <article class="article-card__cruises">
            
                 <figure class="card-content__cruises">
-                        <img src="images/blog-images/family.jpg" alt="family image" loading="lazy">
+                        <img src="images/blog-images/cruise-3.jpg" alt="family image" loading="lazy">
                 </figure>
                 <div>
-                <h2> Australia & New Zealand Cruises from $999! </h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis ullam consequuntur nesciunt fugiat magni explicabo aut similique dicta rerum? Sequi aperiam atque sit odit earum! Facilis perferendis adipisci incidunt? </p>
+                <h2>Expect the Ultimate Travel Adventure </h2>
+                <p> You can sail to the far corners of the globe on our small, luxurious ships, but you’ll always feel like you’re coming home to family. </p>
                             <a href="#" class="btn large-btn">Read More</a>
             </div>
 
