7398b7696aceb049f6804ac561b2e83f299722f9
Brandon Pretelt <brandonpretelt93@gmail.com>
2022-04-25 21:25:57 -0600

    :sparkles:

---
+++ unit10/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <title>HOME || Dreamer's Vacation LLC </title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    
+</head>
+
+<body>
+    <div class="container">
+        <main>
+            <div class="site-head">
+                <header>
+                    <h3> Dreamer's Vacation </h3>
+                </header>
+                <nav>
+                    <ul class="nav">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Test 1</a></li>
+                        <li><a href="#">Test 2</a></li>
+                        <li><a href="#">Test 3</a></li>
+                    </ul>
+                </nav>
+            </div>
